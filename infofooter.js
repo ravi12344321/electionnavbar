55951f870c287b792dfffba346e239fbd6a69334
ravi12344321 <ravi12344321@gmail.com>
2023-08-29 23:34:20 +0530

    Create infofooter.js

---
+++ infofooter.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+export default function Infofooter() {
+  return (
+    <>
+    <div>
+      <footer>
+        <div className="row">
+            <div className="col">
+                <h6>Latest Election</h6>
+                <ul>
+                    <li><a href="/">MCD Elections 2017</a></li>
+                    <li><a href="/">World Elections 2017</a></li>
+                    <li><a href="/">Panjab Result 2017</a></li>
+                    <li><a href="/">Goa Election Result 2017</a></li>
+                    <li><a href="/">Uttarakhand Result 2017</a></li>
+                    <li><a href="/">Latest News Update 2017</a></li>
+                    <li><a href="/">Punjab Result 2017</a></li>
+                </ul>
+            </div>
+            <div className="col">
+                <h6>List of CEO</h6>
+                <ul>
+                    <li><a href="/">CEO Maharashtra</a></li>
+                    <li><a href="/">CEO Jharkhand</a></li>
+                    <li><a href="/">CEO Haryana</a></li>
+                    <li><a href="/">CEO Odisha</a></li>
+                    <li><a href="/">CEO Arunachal Pradesh</a></li>
+                    <li><a href="/">CEO Andhra Pradesh</a></li>
+                    <li><a href="/">UP Election Result 2017</a></li>
+                </ul>
+            </div>
+            <div className="col">
+                <h6>Parliament Constituency</h6>
+                <ul>
+                    <li><a href="/">Andhra Pradesh</a></li>
+                    <li><a href="/">Delhi</a></li>
+                    <li><a href="/">Maharashtra</a></li>
+                    <li><a href="/">Haryana</a></li>
+                    <li><a href="/">Odisha</a></li>
+                    <li><a href="/">Northeast</a></li>
+                </ul>
+            </div>
+            <div className="col">
+                <h6>Assemply Election</h6>
+                <ul>
+                    <li><a href="/">Haryana</a></li>
+                    <li><a href="/">Maharashtra</a></li>
+                    <li><a href="/">Odisha</a></li>
+                    <li><a href="/">Andhra Pradesh</a></li>
+                    <li><a href="/">Arunachal Pradesh</a></li>
+                    <li><a href="/">Jharkhand</a></li>
+                </ul>
+            </div>
+            <div className="col">
+                <h6>Indian Goverment</h6>
+                <ul>
+                    <li><a href="/">President of India</a></li>
+                    <li><a href="/">Chif Minister of India</a></li>
+                    <li><a href="/">Indian Governors</a></li>
+                    <li><a href="/">Union Cabinet Ministers</a></li>
+                    <li><a href="/">Union Cabinet Ministers</a></li>
+                    <li><a href="/">Member Of Parliament</a></li>
+                    <li><a href="/">State Wise Cabinet Ministers</a></li>
+                </ul>
+            </div>
+            <div className="col">
+                <h6>Political News</h6>
+                <ul>
+                    <li><a href="/">LOk Sabha</a></li>
+                    <li><a href="/">Andhra Pradesh</a></li>
+                    <li><a href="/">Andhra Pradesh</a></li>
+                    <li><a href="/">Delhi</a></li>
+                    <li><a href="/">Maharashtra</a></li>
+                    <li><a href="/">More..</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr/>
+        <p className="copyright">All Right Reserved</p>
+    </footer>
+    </div>
+    
+
+    </>
+  )
+}
