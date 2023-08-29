14de5529ebfaca08076c16c98b540a8b08b9029f
ravi12344321 <ravi12344321@gmail.com>
2023-08-29 23:34:52 +0530

    Create grid.js

---
+++ grid.js
@@ -0,0 +1,21 @@
+import React from 'react'
+export default function grid() {
+  return (
+    <>
+  <div>
+    <div className ="wrapper"></div>
+    <ul  className ="one">LEADTECH SERVICES
+    <li> <a className="one"  href="/">Get political survey Done</a></li>
+    <li> <a className="one" href="/">Opinion Poll& Exit Poll</a></li>
+    <li> <a className="one" href="/">Constituency Profilling</a></li>
+    <li> <a className="one"  href="/">Election Campaign Mgmt</a></li>
+    <li> <a className="one"  href="/">Election Result Analysis</a></li>
+    <li> <a className="one" href="/">Social Media Management</a></li>
+    <li> <a className="one" href="/">How to win an election</a></li>
+</ul>
+    </div>
+
+    
+    </>
+  )
+}
